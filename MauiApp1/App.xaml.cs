f4cf1f75e4eaf1740ee34ca5d1c8df96347ee98c
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-09-17 13:09:09 +0300

    Properties_settingUp

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace MauiApp1
+﻿using MauiApp1.Views;
+
+namespace MauiApp1
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override async void OnStart()
+        {
+            base.OnStart();
+
+            MainPage = new NavigationPage(new LoadingPage());
+
+            await Task.Delay(5000);
 
             MainPage = new AppShell();
         }
